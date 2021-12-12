e068d6dc8f3cbf551a95d57f7d8ec24fa93e4d15
mkaywins <maxl.kuttner@gmail.com>
2021-12-12 22:34:21 +0100

    all ex. done - outputfiles

--- HW2/0493_Y2CO_CS2_R_ G1.R
+++ HW2/0493_Y2CO_CS2_R_ G1.R
@@ -99,6 +99,8 @@ df.SP500 = tq_get("^GSPC", get='stock.prices',
                   from="2011-01-01", to="2020-12-31")
 
 
+
+
 # why use arithmetic returns instead of log returns for beta:
 # https://www.researchgate.net/post/What_are_the_pros_and_cons_of_different_methods_to_calculate_asset_returns
 
@@ -168,16 +170,164 @@ df.COP.weekly$beta = beta.weekly
 df.COP.monthly$beta = beta.monthly
 
 
-df.COP.daily = df.COP.daily %>%
-  filter(between(date, as.Date("2016-01-01"),as.Date("2020-12-31")))
+#df.COP.daily = df.COP.daily %>%
+#  filter(between(date, as.Date("2016-01-01"),as.Date("2020-12-31")))
 
-df.COP.weekly = df.COP.weekly %>%
-  filter(between(date, as.Date("2016-01-01"),as.Date("2020-12-31")))
+#df.COP.weekly = df.COP.weekly %>%
+#  filter(between(date, as.Date("2016-01-01"),as.Date("2020-12-31")))
 
-df.COP.monthly = df.COP.monthly %>%
-  filter(between(date, as.Date("2016-01-01"),as.Date("2020-12-31")))
+#df.COP.monthly = df.COP.monthly %>%
+#  filter(between(date, as.Date("2016-01-01"),as.Date("2020-12-31")))
 
-write.csv(df.COP.daily, file = "OutputFiles/COP_beta_daily.csv")
-write.csv(df.COP.weekly, file = "OutputFiles/COP_beta_weekly.csv")
-write.csv(df.COP.monthly, file = "OutputFiles/COP_beta_monthly.csv")
+# ---- Write the data frame into output ---
+#write.csv(df.COP.daily, file = "OutputFiles/COP_beta_daily.csv")
+#write.csv(df.COP.weekly, file = "OutputFiles/COP_beta_weekly.csv")
+#write.csv(df.COP.monthly, file = "OutputFiles/COP_beta_monthly.csv")
+
+
+
+# Another approach to computing the beta is by fitting a linear regression 
+# both approaches are valid
+
+df.riskfree = data.frame(date=df.SP500$date)
+
+
+df.riskfree.monthly = tq_get("TB3MS", get='economic.data',
+                     from="2011-01-01", to="2020-12-31",
+                     period="monthly")
+
+# covert percentage to values
+df.riskfree.monthly$price = df.riskfree.monthly$price / 100 
+
+# create a "last-riskfree rate" data frame
+df.riskfree.daily = df.riskfree %>% left_join(df.riskfree.monthly, by="date")
+df.riskfree.daily = df.riskfree.daily %>%  tidyr::fill(everything(df.riskfree.daily), 
+                                                       .direction = "down")
+colnames(df.riskfree.daily)[3] = "risk_free_rate"
+
+
+# sp500 - daily
+df.SP500.daily = df.SP500.daily %>% left_join(df.riskfree.daily, by="date") 
+df.SP500.daily$excess_return = df.SP500.daily$daily.returns - df.SP500.daily$risk_free_rate
+df.regression.daily = df.SP500.daily %>% left_join(df.COP.daily, by="date") 
+df.regression.daily$excess_return.y = df.regression.daily$daily.returns.y - df.regression.daily$risk_free_rate 
+
+# sp500 - weekly
+df.SP500.weekly = df.SP500.weekly %>% left_join(df.riskfree.daily, by="date") 
+df.SP500.weekly$excess_return = df.SP500.weekly$weekly.returns - df.SP500.weekly$risk_free_rate
+df.regression.weekly = df.SP500.weekly %>% left_join(df.COP.weekly, by="date") 
+df.regression.weekly$excess_return.y = df.regression.weekly$weekly.returns.y - df.regression.weekly$risk_free_rate 
+
+# sp500 - monthly
+df.SP500.monthly = df.SP500.monthly %>% left_join(df.riskfree.daily, by="date") 
+df.SP500.monthly$excess_return = df.SP500.monthly$monthly.returns - df.SP500.monthly$risk_free_rate
+df.regression.monthly = df.SP500.monthly %>% left_join(df.COP.monthly, by="date") 
+df.regression.monthly$excess_return.y = df.regression.monthly$monthly.returns.y - df.regression.monthly$risk_free_rate 
+
+
+df.regression.daily = df.regression.daily %>% select(!beta) %>% na.omit()
+df.regression.daily$regression.beta = rollRegres::roll_regres(data = df.regression.daily,
+                                                formula = "excess_return.y ~ excess_return",
+                                                width = 200)$coef[,2]
+df.regression.daily = df.regression.daily %>% 
+  filter(between(date, as.Date("2016-01-01"),as.Date("2020-12-31"))) %>% 
+  select(date,regression.beta)
+  
+
+df.regression.weekly= df.regression.weekly %>% select(!beta) %>% na.omit()
+df.regression.weekly$regression.beta = rollRegres::roll_regres(data = df.regression.weekly,
+                                                              formula = "excess_return.y ~ excess_return",
+                                                              width = 104)$coef[,2]
+df.regression.weekly = df.regression.weekly %>% 
+  filter(between(date, as.Date("2016-01-01"),as.Date("2020-12-31"))) %>% 
+  select(date, regression.beta)
+
+
+df.regression.monthly= df.regression.monthly %>% select(!beta) %>% na.omit()
+df.regression.monthly$regression.beta = rollRegres::roll_regres(data = df.regression.monthly,
+                                                               formula = "excess_return.y ~ excess_return",
+                                                               width = 60)$coef[,2]
+
+df.regression.monthly = df.regression.monthly %>% 
+  filter(between(date, as.Date("2016-01-01"),as.Date("2020-12-31"))) %>% 
+  select(date, regression.beta)
+
+# merge beta data frames - this is kinda sloppy 
+df.daily_tmp = read.csv("OutputFiles/COP_beta_daily.csv")
+df.daily_tmp$date = df.daily_tmp$date %>% as.Date()
+df.regression.daily = df.regression.daily %>% 
+  left_join(df.daily_tmp, by="date")
+
+rm(df.daily_tmp)
+  
+#write.csv(df.regression.daily %>% select(!X), file = "OutputFiles/COP_beta_daily.csv")
+
+df.weekly_tmp = read.csv("OutputFiles/COP_beta_weekly.csv")
+df.weekly_tmp$date = df.weekly_tmp$date %>% as.Date()
+df.regression.weekly = df.regression.weekly %>% 
+  left_join(df.weekly_tmp, by="date")
+
+rm(df.weekly_tmp)
+
+#write.csv(df.regression.weekly %>% select(!X), file = "OutputFiles/COP_beta_weekly.csv")
+
+
+df.monthly_tmp = read.csv("OutputFiles/COP_beta_monthly.csv")
+df.monthly_tmp$date = df.monthly_tmp$date %>% as.Date()
+df.regression.monthly = df.regression.monthly %>% 
+  left_join(df.monthly_tmp, by="date")
+
+rm(df.monthly_tmp)
+#write.csv(df.regression.monthly %>% select(!X), file = "OutputFiles/COP_beta_monthly.csv")
+
+### Ex. 3 ####
+
+# 3.1 Asset (unlevered) Beta:  E/(E+D) * beta_equity + D/(E+D) * beta_debt
+
+#load("~/WU/CorporateFinance/CorporateFinance/.RData")
+
+
+# filter the weekly betas for the report dates
+# -> we take the weekly betas from exercise 1
+
+price_dates = c("2016-03-31", "2016-06-30", "2016-09-30", "2016-12-30",
+                "2017-03-31", "2017-06-30", "2017-09-29", "2017-12-29",
+                "2018-03-29", "2018-06-29", "2018-09-28", "2018-12-31",
+                "2019-03-29", "2019-06-28", "2019-09-30", "2019-12-31", 
+                "2020-03-31", "2020-06-30", "2020-09-30", "2020-12-31") %>% as.Date()
+
+
+df.results.quarterly$equity_beta = c(NA,NA,NA,NA,
+                                     df.COP.weekly$beta[c(12,25,39,52,
+                                                   65,78,91,104,
+                                                   117,130,143,156,
+                                                   169,182,195,208,
+                                                   221,234,247,261)])
+
+
+# [HARD CODE] the rating from the table in the book 12.3
+df.results.quarterly$debt_rating = c("A-", "A-", "A-", "A-",
+                                     rep("A", 20))
+df.results.quarterly$debt_beta = ifelse(df.results.quarterly$debt_rating == "A", 0.05, 0.1)
+
+# compute the total debt (current_portion_of_long_term_debt is 0 for all quarters)
+df.results.quarterly$total_debt =  df.bs.quarterly$long_term_debt + df.bs.quarterly$short_term_debt
+
+# for completeness add market cap in the df for the quarterly results
+df.results.quarterly$market_cap = market_cap.quarterly$market_cap
+
+
+####Asset or unlevered betas: E/(E+D)*beta_equity+D/(E+D)*beta_debt
+df.results.quarterly$beta_unlevered = df.results.quarterly$market_cap * df.results.quarterly$equity_beta/ (
+  df.results.quarterly$market_cap + df.results.quarterly$total_debt) +
+  df.results.quarterly$total_debt * df.results.quarterly$debt_beta / (
+    df.results.quarterly$market_cap + df.results.quarterly$total_debt)
+
+
+
+write.csv(df.results.quarterly, file = "OutputFiles/COP_quarterly_results.csv")
+
+
+
+
 
--- README.md
+++ README.md
@@ -3,8 +3,8 @@
 ## ToDo:
 * HW2
     + Ex.1 [Done]
-    + Ex.2 [Open]
-    + Ex.3 [Open]
+    + Ex.2 [Done]
+    + Ex.3 [Done]
 
 Note: please don't share with other groups. Thx.
 ![alt text](https://qfinclub.com/static/images/landing-image.jpg)
